b14f0a03b541ff0a9e604e4703fce572680e54ae
jace-ys <jaceys.tan@gmail.com>
2019-01-26 16:53:30 +0000

    json endpoints

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ currentVotes = [
   },
 ]
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Emotivote server!');
