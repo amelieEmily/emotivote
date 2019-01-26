f4e5467724f22c901111a513115b172a1696f235
markgeejw <jw.ziggee@gmail.com>
2019-01-26 18:20:24 +0000

    dockerizing

--- speech/speech.py
+++ speech/speech.py
@@ -1,16 +1,17 @@
 import time, logging
 from datetime import datetime
 import threading, collections, queue, os, os.path
-from halo import Halo
 from google.cloud import speech
 from vad_audio import VADAudio
 from df import DialogflowClient
+import requests
 
 def main():
     # See http://g.co/cloud/speech/docs/languages
     # for a list of supported languages.
     language_code = 'en-US'  # a BCP-47 language ta
     df_client = DialogflowClient()
+    url = "http://localhost:3000"
 
     client = speech.SpeechClient()
     config = speech.types.RecognitionConfig(
@@ -27,16 +28,13 @@ def main():
     frames = vad_audio.vad_collector()
 
     # Stream from microphone to CloudSpeech using VAD
-    spinner = Halo(spinner='line')
-    requests = []
+    stream = []
     
     for frame in frames:
         if frame is not None:
-            spinner.start()
-            requests.append(speech.types.StreamingRecognizeRequest(audio_content=frame))
+            stream.append(speech.types.StreamingRecognizeRequest(audio_content=frame))
         else:
-            spinner.stop()
-            responses = client.streaming_recognize(streaming_config, requests)
+            responses = client.streaming_recognize(streaming_config, stream)
             for response in responses:
                 for result in response.results:
                     alternatives = result.alternatives
@@ -47,9 +45,23 @@ def main():
                         print("Intent: " + intent)
                         if "topic" in entities:
                             print("Topic: " + entities["topic"])
+                            try:
+                                endpoint = url + "/topic"
+                                query = dict()
+                                query["topic"] = entities["topic"]
+                                response = requests.request("POST", endpoint, json=query)
+                            except requests.exceptions.RequestException as e:
+                                logging.error(e)
                         if "ideas" in entities:
                             print("Ideas: " + entities["ideas"])
-            requests = []
+                            try:
+                                endpoint = url + "/suggestions"
+                                query = dict()
+                                query["suggestion"] = entities["ideas"]
+                                response = requests.request("POST", endpoint, json=query)
+                            except requests.exceptions.RequestException as e:
+                                logging.error(e)
+            stream = []
             
 if __name__ == '__main__':
     main()
